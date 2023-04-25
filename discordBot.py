299209ae0c46ab4912af21ec020ac11e62a243b6
Saltyc0der <viljar.berg.jyrgens@gmail.com>
2023-04-25 17:33:47 +0300

    More logging

--- discordBot.py
+++ discordBot.py
@@ -70,6 +70,9 @@ bot = ArmoryBot(command_prefix=getBotPrefix, intents=intents)
 
 @bot.event
 async def on_ready():
+    
+    bot.get_channel(1099702530667204699).send("Hi! I restarted.")
+    bot.get_channel(1099702530667204699).send("I'm currently part of " + len(bot.prefixes) " guilds.")
     await bot.load_extension("Commands.commandFacade")
         
 @bot.event
@@ -91,7 +94,8 @@ async def on_command_error(ctx, error):
     await channel.send(msg)
 
 @bot.event
-async def on_guild_join(guild):
+async def on_guild_join(guild)
+    bot.get_channel(1099702530667204699).send("Hi! I just joined: " + guild.name)
     bot.db.query("INSERT INTO guild (guild_snowflake, realm, prefix) VALUES ({snowflake}, '{realm}', '{prefix}')"
                     .format(snowflake = guild.id, realm = "Icecrown", prefix = ".bot "))
     bot.db.commit()
@@ -99,6 +103,7 @@ async def on_guild_join(guild):
 
 @bot.event
 async def on_guild_remove(guild):
+    bot.get_channel(1099702530667204699).send("Hi! I just left: " + guild.name)
     bot.db.query("DELETE FROM guild WHERE guild_snowflake = {snowflake}"
                     .format(snowflake = guild.id))
 
